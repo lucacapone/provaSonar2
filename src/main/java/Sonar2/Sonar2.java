18b86fde0dee759cffce5d09ae7990b8796fe744
LUCALAM <lucacapo97@gmail.com>
2023-01-12 15:38:36 +0100

    primo correzione

--- src/main/java/Sonar2/Sonar2.java
+++ src/main/java/Sonar2/Sonar2.java
@@ -1,14 +1,15 @@
-public class sonar2 {
+package Sonar2;
+
+public class Sonar2 {
     // metodo somma
     public void somma(int numero){
-        int sommatoria;
-        sommatoria = numero + numero;
+        numero= numero + numero;
     }
 
     public static void main(String args[]){
         int numero;
         numero = 4;
-        sonar2 s = new sonar2();
+        Sonar2 s = new Sonar2();
         s.somma(numero);
         numero = 5;
         s.somma(numero);
